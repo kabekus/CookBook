ae65c7303dac233a2676a1e896f69451e7bd7aa7
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2022-04-12 18:37:38 +0300

    Permission checks completed

--- app/src/main/java/com/kabe/recipesbook/SpecificationFragment.kt
+++ app/src/main/java/com/kabe/recipesbook/SpecificationFragment.kt
@@ -1,12 +1,28 @@
 package com.kabe.recipesbook
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_specification.*
 
 class SpecificationFragment : Fragment() {
+    var selectpic : Uri? = null
+    var selectBitmap : Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,6 +34,74 @@ class SpecificationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_specification, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        saveButton.setOnClickListener {
+            saveButton(it)
+        }
+
+        imageView.setOnClickListener {
+            selectPictures(it)
+        }
+    }
+
+    fun saveButton(view: View){
+        activity?.let {
+            Toast.makeText(it.applicationContext,"Tıklandı",Toast.LENGTH_LONG).show() //Toast Message
+        }
+
+
+    }
+    fun selectPictures(view: View){
+        activity?.let {
+            if(ContextCompat.checkSelfPermission(it.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){ //Kullanıcının galerisine erişim izni
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }else{
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray ) {
+
+        if (requestCode == 1){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val galeriIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent,2)
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       if (requestCode == 2 && requestCode == Activity.RESULT_OK && data != null){
+           selectpic = data.data
+           try {
+               context?.let {
+                   if (selectpic != null){
+                       if (Build.VERSION.SDK_INT >= 28){
+                         val source = ImageDecoder.createSource(it.contentResolver,selectpic!!)
+                           selectBitmap = ImageDecoder.decodeBitmap(source)
+                           imageView.setImageBitmap(selectBitmap)
+                       }else{
+                           selectBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver,selectpic)
+                           imageView.setImageBitmap(selectBitmap)
+                       }
+                   }
+               }
+           }catch (e : Exception){
+               e.printStackTrace()
+           }
+       }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
